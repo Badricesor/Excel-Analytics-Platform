b6ec65c8b24dbafa58d2ba30291ba5c4a75a7fec
Badricesor <badricesor9@gmail.com>
2025-04-24 19:08:39 +0530

    done with basic setup of user dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Signup } from "./pages/Signup.jsx"
+import  Signup  from "./pages/Signup.jsx"
+import  UserDashboard  from './pages/userDashboard.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/Excel-Analytics-Platform/signup" element={<Signup/>} />
+        <Route path="/Excel-Analytics-Platform/dashboard" element={<UserDashboard/>} />
         
       </Routes>
     </Router>
