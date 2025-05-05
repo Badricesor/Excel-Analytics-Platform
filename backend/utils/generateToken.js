5bd8294aa30d03d2d30e27422e4647dcc59fc8de
Badricesor <badricesor9@gmail.com>
2025-05-05 12:13:44 +0530

    fixed token error

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,8 +4,8 @@ import jwt from "jsonwebtoken";
 
 
 export const generateToken= (userId,role)=>{
-    return jwt.sign({ userId, role },
-    process.env.JWT_SECRET, { expiresIn:
-    "20d" })
+    return jwt.sign({ id: userId, role }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+      });
     
 }
