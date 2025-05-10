83997108b83440e64ba25a3af7067bc126f2b40a
Badricesor <badricesor9@gmail.com>
2025-05-10 17:46:21 +0530

    fixing generate chart error

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,53 +1,59 @@
-import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
-const userSchema= mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
-    password:{
-        type: String,
-        required:true,
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
     },
-    role:{
-        type: String,
-        enum:["user","admin"],
-        default:"user"
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
     },
-    uploadHistory: [
-        {
-          fileId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Upload',
-          },
-          analysisDetails: Object, // Store details of analysis performed
-          timestamp: {
-            type: Date,
-            default: Date.now,
-          },
-        },
-      ],
-}
-)
-// userSchema.pre('save', async function (next) {
-//   if (!this.isModified('password')) {
-//     next();
-//   }
-//   const salt = await bcrypt.genSalt(10);
-//   this.password = await bcrypt.hash(this.password, salt);
-// });
+    uploadHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Upload' }],
+    analysisHistory: [
+      {
+        uploadId: { type: mongoose.Schema.Types.ObjectId, ref: 'Upload' },
+        xAxis: String,
+        yAxis: String,
+        chartType: String,
+        timestamp: Date,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
+// Method to compare entered password with the hashed password in the database
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
-const User= mongoose.model("User",userSchema)
+// Conditional model definition to prevent OverwriteModelError
+let User;
+
+if (mongoose.models.User) {
+  // If the 'User' model has already been compiled, use the existing one
+  User = mongoose.model('User');
+} else {
+  // If the 'User' model has not been compiled yet, define and compile it
+  User = mongoose.model('User', userSchema);
+}
 
 export default User;
