a2e9c77bd37296a02bd4c9b211017f3dbed0bda2
Badricesor <badricesor9@gmail.com>
2025-05-24 11:40:26 +0530

    fixing backend

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,18 @@ const app= express()
 
 // Configure CORS to allow requests from your frontend's origin
 const corsOptions = {
-    origin: '*',
+  // It's highly recommended to uncomment and use the more secure dynamic origin check for production
+  origin: (origin, callback) => {
+      console.log('Request Origin:', origin); 
+      const allowedOrigins = ['http://localhost:5173', 'https://excel-analytics-platform.onrender.com',"https://excel-analytics-platform-frontend-w56n.onrender.com"]; 
+      if (allowedOrigins.includes(origin) || !origin) { 
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
- 
+  credentials: true,
   allowedHeaders: 'Content-Type, Authorization, *',
 };
 
